d9e3ea61ed29aa0e98994e36b502ac587c09ffe4
OwaisBukhari <owaisbukhari257@gmail.com>
2021-12-03 19:10:18 +0500

    With Foreground Service[MODIFIED]

--- app/src/main/java/com/mafaz/backgroundmusicplayer/MainActivity.java
+++ app/src/main/java/com/mafaz/backgroundmusicplayer/MainActivity.java
@@ -1,20 +1,25 @@
 package com.mafaz.backgroundmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button start;
     Button stop;
+    EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input=(EditText)findViewById(R.id.inputtext);
 
          start = (Button) findViewById(R.id.start);
          stop = (Button)findViewById(R.id.stop);
@@ -27,13 +32,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
         if(view==start){
-            startService(new Intent(this,musicService.class));
+            ////startService(new Intent(this,musicService.class));
+            startService();
 
 
         }else if(view==stop){
-            stopService(new Intent(this,musicService.class));
+           // stopService(new Intent(this,musicService.class));
+            stopService();
         }
 
 
+    }
+
+    public void startService() {
+        String inputdata= String.valueOf(input.getText());
+        Intent serviceIntent = new Intent(this, musicService.class);
+        serviceIntent.putExtra("inputExtra", inputdata);
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+    public void stopService() {
+        Intent serviceIntent = new Intent(this, musicService.class);
+        stopService(serviceIntent);
     }
 }
--- app/src/main/java/com/mafaz/backgroundmusicplayer/musicService.java
+++ app/src/main/java/com/mafaz/backgroundmusicplayer/musicService.java
@@ -1,16 +1,25 @@
 package com.mafaz.backgroundmusicplayer;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 import android.provider.Settings;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 public class musicService extends Service {
      MediaPlayer player;
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
+
 
 
     @Override
@@ -18,10 +27,43 @@ public class musicService extends Service {
        //  MediaPlayer player;
 
 
-        player= MediaPlayer.create(this,Settings.System.DEFAULT_NOTIFICATION_URI);
-        player.setLooping( true );
-        player.start();
-        return START_STICKY;
+        //player= MediaPlayer.create(this,Settings.System.DEFAULT_NOTIFICATION_URI);
+        //player.setLooping( true );
+        //player.start();
+       // return START_STICKY;
+        String input = intent.getStringExtra("inputExtra");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel();
+        }
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Foreground Service")
+                .setContentText(input)
+               // .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+        //do heavy work on a background thread
+        //stopSelf();
+        return START_NOT_STICKY;
+
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
 
     }
 

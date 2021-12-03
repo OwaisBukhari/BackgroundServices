2038637ebd3dbf22c60746aa65d7bf5adc9dc073
OwaisBukhari <owaisbukhari257@gmail.com>
2021-12-03 17:18:57 +0500

    notification sounds in background

---
+++ app/src/main/java/com/mafaz/backgroundmusicplayer/MainActivity.java
@@ -0,0 +1,39 @@
+package com.mafaz.backgroundmusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button start;
+    Button stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         start = (Button) findViewById(R.id.start);
+         stop = (Button)findViewById(R.id.stop);
+
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view==start){
+            startService(new Intent(this,musicService.class));
+
+
+        }else if(view==stop){
+            stopService(new Intent(this,musicService.class));
+        }
+
+
+    }
+}
